d1ee36fc89d66f3a930b72418adfebe8efbc5317
Hiccup <nopenigita@163.com>
2019-05-06 23:46:12 +0800

    modify

--- src/cn/itcast/test/CalculatorTest.java
+++ src/cn/itcast/test/CalculatorTest.java
@@ -13,7 +13,6 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         int result = calculator.add(1,2);
         Assert.assertEquals(3,result);
-        System.out.println();
-
+        System.out.println(1);
     }
 }
